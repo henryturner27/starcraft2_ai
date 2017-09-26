dc74c7355e631c7bf45dcefbec42610d7dcbe609
henryturner27 <turnerh27@MacBook-Pro-2.local>
2017-09-25 20:56:50 -0700

    change run_agent to 1000 steps

--- starcraft2_ai/run_agent.py
+++ starcraft2_ai/run_agent.py
@@ -39,7 +39,7 @@ class RunAgent(object):
         self.replay_dir = platform_settings.replay_dir
 
     def run_agent(self):
-        steps = 2000
+        steps = 1000
         step_mul = 8
         with sc2_env.SC2Env(
                 map_name=self.map_name,
